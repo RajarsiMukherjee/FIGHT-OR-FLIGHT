921c983968b16f8834628f2cbf350384e988e9f6
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-04-28 14:38:13 +0530

    all redux functionalities added

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 export default function Home() {
 
     const [from, setFrom] = React.useState('');
     const [to, setTo] = React.useState('');
+    const [data, setData] = useState([])
+    const [disp, setDisp] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/Flights").then((res) => setData(res.data))
+    }, [])
+
+    const handleSearch = () => {
+        const disp1 = data.filter((e) => {
+            // console.log("eee",e)
+            if ((from == e.from) && (to == e.to)) {
+                return e
+            }
+        })
+        setDisp(disp1)
+        console.log("disp", disp1)
+        console.log("apend", disp)
+    }
 
 
     return (
         <div>
             <div>
+
                 <Box
                     component="form"
                     sx={{
@@ -26,19 +46,54 @@ export default function Home() {
                         id="outlined-name"
                         label="FROM"
                         // value={name}
-                        onChange={(e)=> {setFrom(e.target.value)}}
+                        onChange={(e) => { setFrom(e.target.value) }}
                     /><br />
 
                     <TextField
                         id="outlined-name"
                         label="TO"
                         // value={name}
-                        onChange={(e)=> {setTo(e.target.value)}}
-                    /><br/>
+                        onChange={(e) => { setTo(e.target.value) }}
+                    /><br />
 
-                    <Button variant="contained">Search</Button>
+                    <Button variant="contained" onClick={handleSearch}>Search</Button>
                 </Box>
             </div>
+
+            <div>
+
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>Cost</th>
+                        <th>From Time</th>
+                        <th>To Time</th>
+                        <th>PNR</th>
+                        <th>Capacity</th>
+                    </tr>
+
+                    {
+                        disp.map((e) => (
+                            <tr key={e.id}>
+                                <td>{e.airlines}</td>
+                                <td>{e.from}</td>
+                                <td>{e.to}</td>
+                                <td>{e.cost}</td>
+                                <td>{e.FromTime}</td>
+                                <td>{e.ToTime}</td>
+                                <td>{e.pnr}</td>
+                                <td>{e.capacity}</td>
+                            </tr>
+                        ))
+                    }
+
+
+
+                </table>
+            </div>
+
         </div>
     )
 }
